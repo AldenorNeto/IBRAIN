9d8aedeb08b6f99983ede437364e085be6a7530c
Aldenor Arruda Gomes Neto <arruda.neto@grendene.com.br>
2024-01-18 08:33:20 -0300

    feat: Adicionando um botão para teste de notificações

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from "expo-notifications";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handleNotification = async () => {
+    await Notifications.presentNotificationAsync({
+      title: "Notificação",
+      body: "A notificação foi pressionada agora!",
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status !== "granted") {
+        await Notifications.requestPermissionsAsync();
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.nosso}>Tudo nosso</Text>
+      <Text style={styles.nosso}>Hello, World!</Text>
+      <Button title="Enviar Notificação" onPress={handleNotification} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,13 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   nosso: {
-    color: '#fff',
-  }
+    color: "#fff",
+  },
 });
